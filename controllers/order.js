18bf80d84b539dab34bc9da44d8670e374041a96
Naved Ahmad <naved@babydestination.com>
2020-08-15 19:56:02 +0530

    added locking mechanism

--- controllers/order.js
+++ controllers/order.js
@@ -1,44 +1,57 @@
+const Mutex = require('async-mutex').Mutex;
+
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 
 const Order = require('../models/Order');
 const Inventory = require('../models/Inventory');
 
+const mutex = new Mutex();
+
 
 // @desc      Create new Order
 // @route     POST /api/v1/order
 // @access    Private
 exports.createOrder= asyncHandler(async (req, res, next) => {
+  const release = await mutex.acquire();
   const account = req.account._id;
   const { inventory, product, description, quantity, price, shippingAddress } = req.body;
   if (!inventory || ! (typeof inventory === 'string')) {
+    release();
     return next(new ErrorResponse('Inventory id is required and should be a string', 400));
   }
   if (!product || ! (typeof product === 'string')) {
+    release();
     return next(new ErrorResponse('Product name is required and should be a string', 400));
   }
   if (!description || ! (typeof description === 'string')) {
+    release();
     return next(new ErrorResponse('Product description is required and should be a string', 400));
   }
   if (!quantity || ! (typeof quantity === 'number')) {
+    release();
     return next(new ErrorResponse('Product quantity is required and should be a number', 400));
   }
   if (!price || ! (typeof price === 'number')) {
+    release();
     return next(new ErrorResponse('Product quantity is required and should be a number', 400));
   }
   if (!shippingAddress || ! (typeof shippingAddress === 'string')) {
+    release();
     return next(new ErrorResponse('Product shipping address is required and should be a string', 400));
   }
   const amount = quantity * price;
   const inventoryItem = await Inventory.findById(inventory);
 
   if (!inventoryItem) {
+    release();
     return next(new ErrorResponse('Inventory does not exist', 404));
   }
 
   const { quantityInStock } = inventoryItem;
 
   if (quantityInStock < quantity) {
+    release();
     return next(new ErrorResponse('Inventory item is out of stock', 400));
   } else {
     inventoryItem.quantityInStock -= 1;
@@ -55,6 +68,8 @@ exports.createOrder= asyncHandler(async (req, res, next) => {
       shippingAddress,
       amount
     });
+
+    release();
 
     res.status(201).json({
       success: true,
